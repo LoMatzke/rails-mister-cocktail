5611f6058bda980974bfb143bf4a8c551e11bca9
Loretta Matzke <lorettamatzke@gmail.com>
2021-02-18 13:03:41 +0100

    Added all validations & associations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
+  validates :cocktail, uniqueness:  { :scope => :ingredient}
+  validates :description, :cocktail, :ingredient, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
 end
